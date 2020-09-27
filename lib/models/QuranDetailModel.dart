f62ea1b774e53f4206cf84969d0acc60fe673d70
Ihsan Nurul Habib <ihsan.inh@gmail.com>
2020-09-27 19:01:49 +0700

    Create Model

---
+++ lib/models/QuranDetailModel.dart
@@ -0,0 +1,39 @@
+class QuranDetailModel {
+  int ayaId;
+  int ayaNumber;
+  String ayaText;
+  int suraId;
+  int juzId;
+  int pageNumber;
+  String translationAyaText;
+
+  QuranDetailModel({
+    this.ayaId,
+    this.ayaNumber,
+    this.ayaText,
+    this.suraId,
+    this.juzId,
+    this.pageNumber,
+    this.translationAyaText
+  });
+
+  factory QuranDetailModel.fromJson(Map<String, dynamic> json) => QuranDetailModel(
+    ayaId: json['aya_id'],
+    ayaNumber: json['aya_number'],
+    ayaText: json['aya_text'],
+    suraId: json['sura_id'],
+    juzId: json['juz_id'],
+    pageNumber: json['page_number'],
+    translationAyaText: json['translation_aya_text'],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "aya_id": ayaId,
+    "aya_number": ayaNumber,
+    "aya_text": ayaText,
+    "sura_id": suraId,
+    "juz_id": juzId,
+    "page_number": pageNumber,
+    "translation_aya_text": translationAyaText,
+  };
+}
---
+++ lib/models/QuranListModel.dart
@@ -0,0 +1,31 @@
+class QuranListModel {
+  int id;
+  String suratName;
+  String suratText;
+  String suratTerjemahan;
+  int countAyat;
+
+  QuranListModel({
+    this.id,
+    this.suratName,
+    this.suratText,
+    this.suratTerjemahan,
+    this.countAyat
+  });
+
+  factory QuranListModel.fromJson(Map<String, dynamic> json) => QuranListModel(
+    id: json['id'],
+    suratName: json['surat_name'],
+    suratText: json['surat_text'],
+    suratTerjemahan: json['surat_terjemahan'],
+    countAyat: json['count_ayat'],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "id": id,
+    "suratName": suratName,
+    "suratText": suratText,
+    "suratTerjemahan": suratTerjemahan,
+    "countAyat": countAyat
+  };
+}
